47705ecc8b110aae53525fc1748f0b5566189cd6
Robert Manea <robert1manea@gmail.com>
2017-05-02 19:44:34 +0300

    added progressBar, resign Keyboard

--- app/src/main/java/com/example/rober/weatherapp/SearchActivity.java
+++ app/src/main/java/com/example/rober/weatherapp/SearchActivity.java
@@ -1,16 +1,20 @@
 package com.example.rober.weatherapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,13 +32,22 @@ public class SearchActivity extends AppCompatActivity {
     private ArrayList<Location> mListCustom;
     private SavedLocationArrayAdapter mCustomArrayAdapter;
     public ListView searchListView;
+    private EditText searchTextView;
     private LocationsHelper mHelper;
+    private ProgressBar mProgress;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
+        mProgress.setIndeterminate(true);
+        mProgress.setVisibility(View.GONE);
+
+        searchTextView = (EditText) findViewById(R.id.searchTextView);
+
         mHelper = new LocationsHelper(this);
 
         searchListView = (ListView) findViewById(R.id.searchLocationsList);
@@ -62,6 +75,8 @@ public class SearchActivity extends AppCompatActivity {
         searchButton.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
+                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(searchTextView.getWindowToken(), 0);
                         searchByName();
                     }
                 }
@@ -75,9 +90,9 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     protected void searchByName() {
-        EditText searchTextView = (EditText) findViewById(R.id.searchTextView);
+        mProgress.setVisibility(View.VISIBLE);
         final Uri builtUri = Uri.parse(JSONReader.HOST+"weather").buildUpon().appendQueryParameter("q",searchTextView.getText().toString()).build();
-        //TODO PROGRESSBAR
+
         new Thread(new Runnable() {
             public void run() {
                 try {
@@ -92,9 +107,24 @@ public class SearchActivity extends AppCompatActivity {
                                 }
                             });
                         } else {
-                            JSONArray jArray = new JSONArray(json);
+                            final JSONArray jArray = new JSONArray(json);
+                            for(int i=0; i<jArray.length(); i++)
+                            {
+                                final JSONObject jsonObj = jArray.getJSONObject(i);
+                                runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        addLocationToList(jsonObj);
+                                    }
+                                });
+                            }
                         }
                     }
+                    runOnUiThread(
+                            new Runnable() {
+                                public void run() {
+                                    mProgress.setVisibility(View.GONE);
+                                }}
+                    );
                 } catch (IOException e) {
                     e.printStackTrace();
                 } catch (JSONException e) {
